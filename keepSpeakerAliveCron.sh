1836996fda497d9ab68f1fb62ef5e9e045adf817
intdel <b.christopher.92@gmail.com>
2015-11-20 10:43:49 -0800

    Fixed bugs in cron script

--- keepSpeakerAliveCron.sh
+++ keepSpeakerAliveCron.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-date >> /tmp/KeepSoundAlive.log
+date >> /tmp/KeepSpeakerAlive.log
 echo "Playing sound..." >> /tmp/KeepSpeakerAlive.log
 afplay Barely\ Audible.mp3 >> /tmp/KeepSpeakerAlive.log
-sleep 1200
 
